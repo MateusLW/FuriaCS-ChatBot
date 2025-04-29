f6491b79af5f215fa565bc6376d8fefbc30874b0
MateusLW <mateuslimaw@gmail.com>
2025-04-29 20:50:27 -0300

    Update Main.java

--- src/main/java/br/com/furiachatbotcs/Main.java
+++ src/main/java/br/com/furiachatbotcs/Main.java
@@ -2,12 +2,19 @@ package br.com.furiachatbotcs;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import com.sun.net.httpserver.HttpServer;
 
 public class Main 
 {
     @SuppressWarnings("CallToPrintStackTrace")
     public static void main(String[] args) 
     {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            exchange.sendResponseHeaders(200, 0);
+            exchange.getResponseBody().close();
+        });
+        server.start();
         try 
         {
             System.out.println("Iniciando bot...");
